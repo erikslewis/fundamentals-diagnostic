1d7b291899279dacf5ff5dfb0e27ecf5966f1041
erikslewis <adelina198527@gmail.com>
2016-11-14 14:13:39 -0500

    Add files via upload

---
+++ diagnostic.js
@@ -0,0 +1,44 @@
+1.
+/Users/grace/wdiGraces-MacBook-Pro:~ grace$ cd wdiGraces-MacBook-Pro:wdi grace$ pwd
+Graces-MacBook-Pro:wdi grace$ mkdir try
+Graces-MacBook-Pro:try grace$ ls
+
+
+
+2.Describe GitHub, briefly in your own words.
+*git hub is like a iCloud that stores all your work and the changes you have made to for files and you can share the links with other so they can see what you have been doing.
+
+3.(false || true) && (false && true);
+ *false we evalute the exspression from left to right
+
+4.var x = 1;
+while (x < 5) {
+  x = x + x;
+} 
+*5 
+5.var strangeAdd = function(a, b) {
+  if (a > 0 || b > 0) {
+    return a + b;
+  }
+  return a - b;
+};
+
+var result = strangeAdd(0, -5);
+*5times   x=2
+   
+6. var groceryList = [
+  'a loaf a bread',
+  'a container of milk',
+  'a stick of butter',
+];
+
+*a stick of butter
+7.var teamMember = {
+  role: 'consultant',
+  name: {
+    given: 'Jason',
+    surname: 'Weeks'
+  },
+  hobby: 'exercise'
+};
+*exercise ... given surname...  jason, weeks
